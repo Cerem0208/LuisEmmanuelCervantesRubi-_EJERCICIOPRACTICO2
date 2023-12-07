e7221b6a1179ef7b3df1993cbad0acde118d29fa
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-12-06 18:25:52 -0600

    commit 1
    proyecto creado, pom modificado, application properties y conexión con DB

---
+++ LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2/src/main/java/com/LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2/Emmanuel/EmmanuelApplication.java
@@ -0,0 +1,13 @@
+package com.LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2.Emmanuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmanuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmanuelApplication.class, args);
+	}
+
+}
---
+++ LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2/src/test/java/com/LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2/Emmanuel/EmmanuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2.Emmanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmanuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
