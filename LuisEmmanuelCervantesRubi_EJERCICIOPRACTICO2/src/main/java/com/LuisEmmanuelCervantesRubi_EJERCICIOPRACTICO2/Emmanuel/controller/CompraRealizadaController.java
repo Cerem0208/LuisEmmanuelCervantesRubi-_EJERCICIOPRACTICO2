04991850e53896d0f15dca26542e71303cee83ce
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-12-07 02:26:32 -0600

    commit 11
    compra realizada añadido

---
+++ LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2/src/main/java/com/LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2/Emmanuel/controller/CompraRealizadaController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.LuisEmmanuelCervantesRubi_EJERCICIOPRACTICO2.Emmanuel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/compra") //nombre de la carpeta
+public class CompraRealizadaController {
+    @GetMapping("/CompraRealizada") //nombre del html
+    public String acercade() {
+
+        return "/compra/CompraRealizada"; //Ruta completa
+    }
+}
+
